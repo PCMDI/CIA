beab3fb33002ccbccc27d7a0b5f7d10c38a2257a
gleckler1 <pgleckler@llnl.gov>
2018-01-11 07:40:55 -0800

    Update writejson.py

--- src/writejson.py
+++ src/writejson.py
@@ -9,10 +9,10 @@ ArgDefaults["--mp"] = {'type':'str', 'dest':'modpath','default':'hi','help':'Exp
 
 # Write json
 
-if os.path.exists('../Defaults/DefArgs.json'):
-    print 'File existing, purging: DefArgs.json'
-    os.remove('../Defaults/DefArgs.json')
-fH = open('../Defaults/DefArgs.json','w')
+if os.path.exists('../Defaults/DefArgsCIA.json'):
+    print 'File existing, purging: DefArgsCIA.json'
+    os.remove('../Defaults/DefArgsCIA.json')
+fH = open('../Defaults/DefArgsCIA.json','w')
 
 json.dump(ArgDefaults,fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
 fH.close()
