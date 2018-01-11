12813897da3b299baa753354e8b3fa618ad3f367
Peter Gleckler <gleckler1@llnl.gov>
2018-01-10 16:45:58 -0800

    DefArgs.json added

--- src/writejson.py
+++ src/writejson.py
@@ -1,1 +1,19 @@
+import os
 import json
+
+
+ArgDefaults = {}
+
+
+ArgDefaults["--mp"] = {'type':'str', 'dest':'modpath','default':'hi','help':'Explicit path to model data'}
+
+# Write json
+
+if os.path.exists('../Tables/DefArgs.json'):
+    print 'File existing, purging: DefArgs.json'
+    os.remove('../Tables/DefArgs.json')
+fH = open('../Tables/DefArgs.json','w')
+
+json.dump(ArgDefaults,fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
+fH.close()
+
