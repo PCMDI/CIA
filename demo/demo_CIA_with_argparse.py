9240f8a27a9d71cd6929dd9a9b734f9e40ca1efa
Peter Gleckler <gleckler1@llnl.gov>
2018-02-12 13:41:44 -0800

    added test using argparse only

---
+++ demo/demo_CIA_with_argparse.py
@@ -0,0 +1,27 @@
+import argparse
+import json
+import os
+import sys
+import ast
+
+p = argparse.ArgumentParser()
+
+fjson = open(os.path.join(sys.prefix,"share","cia","DefArgsCIA.json"))
+
+json_data = json.load(fjson)
+
+for key in json_data:
+    if key[0]!='-': continue
+    params = json_data[key]
+    if "aliases" in params:
+        option_strings = params.pop("aliases")
+    else:
+        option_strings = []
+    option_strings.insert(0,key)
+    params["type"] = eval(params.pop("type", "str"))
+    p.add_argument(*option_strings, **params)
+
+args = p.parse_args(sys.argv[1:])
+
+print args
+
