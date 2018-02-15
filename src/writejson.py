8d69beb98006a439d8ecf78b4f305c0e848aa053
Peter Gleckler <gleckler1@llnl.gov>
2018-02-15 14:01:09 -0800

    defaults now are longnames, added MPI options

--- src/writejson.py
+++ src/writejson.py
@@ -6,17 +6,22 @@ import json
 #Populate default arguments
 ArgDefaults = {}
 
-ArgDefaults["-p"] = {'aliases':["--parameter"]}
-ArgDefaults["--mp"] = {'aliases':['--modpath'],'type':'str', 'dest':'modpath','default':'hi','help':'Explicit path to model data'}
-ArgDefaults["--mns"] = {'aliases':['--modnames'],'type':'ast.literal_eval', 'dest':'modnames','default':'None','help':'A list of names that can be used to loop through modpath'}
+ArgDefaults["--parameter"] = {'aliases':["-p"]}
+ArgDefaults["--modpath"] = {'aliases':['--mp'],'type':'str', 'dest':'modpath','default':'hi','help':'Explicit path to model data'}
+ArgDefaults["--modnames"] = {'aliases':['--mns'],'type':'ast.literal_eval', 'dest':'modnames','default':'None','help':'A list of names that can be used to loop through modpath'}
 
-ArgDefaults["--rd"] = {'aliases':['--results_dir'],'type':'ast.literal_eval', 'default':'None','help':'The name of the folder where all runs will be stored.'}
-ArgDefaults["--cid"] = {'aliases':['--case_id'],'dest':'modnames','default':'None','help':'The name of the subdirectory (below results_dir where results from a paritcular code execution is stored '}
+ArgDefaults["--results_dir"] = {'aliases':['--rd'],'type':'ast.literal_eval', 'default':'None','help':'The name of the folder where all runs will be stored.'}
+ArgDefaults["--case_id"] = {'aliases':['--cid'],'dest':'modnames','default':'None','help':'The name of the subdirectory (below results_dir where results from a paritcular code execution is stored '}
 
-ArgDefaults['--rd'] = {'aliases':['--reference_data_path'],'help':'The path/filename of reference (obs) data.'}
+ArgDefaults['--reference_data_path'] = {'aliases':['--rd'],'help':'The path/filename of reference (obs) data.'}
 
-ArgDefaults['--tdp'] = {'aliases':['--test_data_path'],'help':'The path/filename to model output.'}
+ArgDefaults['--test_data_path'] = {'aliases':['--tp'],'help':'The path/filename to model output.'}
 
+ArgDefaults['--diags'] = {'aliases':["-d"],'help':"Path to other user-defined parameter file.","help":"Path to other user-defined parameter file.","type":"str","nargs":"+","required":False,"dest":"other_parameters"}
+
+ArgDefaults['--num_workers'] = {'aliases':["-n"],'help':"Number of workers, used when running with multiprocessing or in distributed mode.","type":"int","required":False}
+
+ArgDefaults['--scheduler_addr"'] = {'aliases':["N/A"],'help':"Address of scheduler in the form of IP_ADDRESS:PORT. Used when running in distributed mode.","type":"str","required":False}
 
 
 # Write arguments json
@@ -29,3 +34,26 @@ fH = open('../Defaults/DefArgsCIA.json','w')
 json.dump(ArgDefaults,fH,ensure_ascii=True,sort_keys=True,indent=4,separators=(',',':'),encoding="utf-8")
 fH.close()
 
+
+
+"""
+        "--diags": {
+                "aliases": ["-d"],
+                "type": "str",
+                "nargs": "+",
+                "dest": "other_parameters",
+                "default": [],
+                "help": "Path to other user-defined parameter file.",
+                "required": false
+        },
+        "--num_workers": {
+                "aliases": ["-n"],
+                "type": "int",
+                "help": "Number of workers, used when running with multiprocessing or in distributed mode.",
+                "required": false
+        },
+        "--scheduler_addr": {
+                "type": "str",
+                "help": "Address of scheduler in the form of IP_ADDRESS:PORT. Used when running in distributed mode."
+        }
+"""
